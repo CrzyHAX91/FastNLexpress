afe0890e55e62827fb7f15cb0cacdc8b888796ea
D C <worlddre3mer1991@protonmail.ch>
2025-01-08 13:56:30 +0100

    Review and improve

--- dropship_project_backup/main_app/utils_old.py
+++
@@ -1,5 +0,0 @@
-def calculate_total_price(cart_items):
-    """
-    Calculate the total price of the cart items.
-    """
-    return cart_items.aggregate(total=Sum(F('product__price') * F('quantity')))['total'] or 0
